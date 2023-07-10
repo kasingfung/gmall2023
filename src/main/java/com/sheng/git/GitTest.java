97c17619506f6fc677b40a041ee57e31d8b07a76
sheng <sheng@holax.com>
2023-07-10 16:11:55 +0800

    third commit

--- src/main/java/com/sheng/git/GitTest.java
+++ src/main/java/com/sheng/git/GitTest.java
@@ -12,7 +12,8 @@ package com.sheng.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello github");
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
